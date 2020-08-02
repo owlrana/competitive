5bd1d19fcf29e4cb08f99e4bbd686b37b58f1350
reachrahulrana <me.rahulrana@outlook.com>
2020-08-02 23:10:29 +0530

    updated

--- rahulrana/complex_numbers.py
+++ rahulrana/complex_numbers.py
@@ -1,26 +1,31 @@
 # https://www.hackerrank.com/challenges/class-1-dealing-with-complex-numbers/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
+import numpy
+
 inp1 = input()
 inp2 = input()
 
 #assigning values of real [1] and imaginary [2] parts into float variables
 real1 = float(inp1.split()[0])
 real2 = float(inp2.split()[0])
-imag1 = float(inp1.split()[1])
-imag2 = float(inp2.split()[1])
+imag1 = float(inp1.split()[1])*1j
+imag2 = float(inp2.split()[1])*1j
+
+num1 = real1 + imag1
+num2 = real2 + imag2
 
 #sanity check to see if the code is working
-print(real1,'+',imag1,'i')
-print(real2,'+',imag2,'i')
+print(num1)
+print(num2)
 
-#adding the numbers
-print(real1+real2, '+', imag1+imag2, 'i')
+def complex_print(a):
+    if a.imag >=0:
+        sign = '+'
+    elif a.imag < 0:
+        sign = '-'
+    print(a.real, sign, abs(a.imag), '\bi')
 
-#subtracting the numbers
-print(real1-real2, '-', imag1-imag2, 'i')
-
-#multplying the numbers
-print(real1*real2, '*', imag1*imag2, 'i')
-
-#dividing the numbers
-print(real1/real2, '/', imag1/imag2, 'i')
+complex_print(num1+num2)
+complex_print(num1-num2)
+complex_print(numpy.around(num1*num2, 2))
+complex_print(numpy.around(num1/num2, 2))
