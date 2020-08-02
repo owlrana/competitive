58b02dd0270623f99e09d97bc81fe47231ad2e68
reachrahulrana <me.rahulrana@outlook.com>
2020-08-02 22:14:27 +0530

    updated

---
+++ rahulrana/0_to_n_print.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-print/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+n = int(input())
+
+for var in range(1, n+1):
+    print(var, end ='')
