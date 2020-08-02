060fd89e05d625f300635985f18511dcdad4227b
reachrahulrana <me.rahulrana@outlook.com>
2020-08-03 00:35:41 +0530

    update

--- rahulrana/complex_numbers.py
+++ rahulrana/complex_numbers.py
@@ -1,4 +1,5 @@
 # https://www.hackerrank.com/challenges/class-1-dealing-with-complex-numbers/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# install numpy manually using pip3, if using python3 
 
 import numpy
 
@@ -15,8 +16,8 @@ num1 = real1 + imag1
 num2 = real2 + imag2
 
 #sanity check to see if the code is working
-print(num1)
-print(num2)
+#print(num1)
+#print(num2)
 
 def complex_print(a):
     if a.imag >=0:
@@ -29,3 +30,5 @@ complex_print(num1+num2)
 complex_print(num1-num2)
 complex_print(numpy.around(num1*num2, 2))
 complex_print(numpy.around(num1/num2, 2))
+complex_print(numpy.around(abs(num1), 2))
+complex_print(numpy.around(abs(num2), 2))
