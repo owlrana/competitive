1b3cb34cd8a728cdee7c1080d9c0e3c606fb69f2
reachrahulrana <me.rahulrana@outlook.com>
2020-08-03 10:43:27 +0530

    update

---
+++ rahulrana/runner-up.py
@@ -0,0 +1,26 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+#take the input
+num = int(input())
+
+#analyse the input
+while num < 2 or not num < 10:    
+    num = int(input())
+
+scores = input()    #input the scores with space bar in b/w
+score_list = scores.split()  #store the scores into a list, remember: the values are still strings
+
+#initialize the highest and runner ups
+high = 0
+runner = -1
+
+#loop to check and assign the values
+for i in range(num):
+    if int(score_list[i]) > high:
+        runner = high
+        high = int(score_list[i])
+    if int(score_list[i]) > runner:
+            if int(score_list[i]) < high:
+                runner = int(score_list[i])
+        
+print(runner)
