2b2f6c824b5636046c59028f0f03dc64942fee90
reachrahulrana <me.rahulrana@outlook.com>
2020-08-04 00:56:18 +0530

    updated

---
+++ rahulrana/check.py
@@ -0,0 +1,12 @@
+#Practice file to check code while taking lectures and notes. Code available for the last lecture and last check only.
+
+man = {
+    'brain' : 85,
+    'voice' : 90,
+    'body' : 50,
+    'happy' : 20
+}
+
+man.body: 5
+
+print(man)
