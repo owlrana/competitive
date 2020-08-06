90c861098a2bde916db2f09c141204fca2b811b6
reachrahulrana <me.rahulrana@outlook.com>
2020-08-06 09:35:54 +0530

    updated: Python for Web Servers

---
+++ rahulrana/Network-Data/sockets.py
@@ -0,0 +1,6 @@
+# to talk to a network web server (usually on port 80)
+# this code does not send any data, just dials the phone
+
+import socket
+mysock = socket.socket(socket.AD_INET, socket.SOCK_STREAM) #socket.SOCK_STREAM = series of characters incoming outgoing
+mysock.connect( ('data.pr4e.prg', 80) ) #data.pr4e.prg is the host (domain name), 80 is the port
