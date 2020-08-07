2d38a8241a1263f2f97a05d882db08a5c1e16ace
reachrahulrana <me.rahulrana@outlook.com>
2020-08-07 22:53:34 +0530

    upgrade

--- rahulrana/Network-Data/ord-function.py
+++ rahulrana/Network-Data/ord-function.py
@@ -1,6 +1,7 @@
 #It's kind of recently that the world throughout talks to each other through UNICODE
 #So we dont have to follow just ASCII 128 or Japanese 
 #happened around 2000s due to the internet boom
+#UTF-8 is best method to encode data within networks
 
 print(ord('H'))
 print(ord('h'))
