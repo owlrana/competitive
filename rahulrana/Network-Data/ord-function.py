fb726e0144bebea3bc6bb5b6cbeb5e097907c7dd
reachrahulrana <me.rahulrana@outlook.com>
2020-08-07 22:44:17 +0530

    updated

--- rahulrana/Network-Data/ord-function.py
+++ rahulrana/Network-Data/ord-function.py
@@ -1,1 +1,7 @@
+#It's kind of recently that the world throughout talks to each other through UNICODE
+#So we dont have to follow just ASCII 128 or Japanese 
+#happened around 2000s due to the internet boom
+
 print(ord('H'))
+print(ord('h'))
+
