cb3786a02c62d921b2d129ce123777336f1e461c
reachrahulrana <me.rahulrana@outlook.com>
2020-08-08 00:13:21 +0530

    update: beautiful soup 4 dwnld lnk+.zip for lcl

--- rahulrana/Network-Data/urllib.py
+++ rahulrana/Network-Data/urllib.py
@@ -1,8 +1,9 @@
-#Using libraries that does all the socket work and makes web pages look more like a file
+# Using libraries that does all the socket work and makes web pages look more like a file
+# These require BeautifulSoup to be downloaded, which can be done from: https://pypi.python.org/pypi/beautifulsoup4
+# or can be locally installed through https://www.py4e.com/code3/bs4.zip and extracted into the local folder
 
 import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().strip())
-    
