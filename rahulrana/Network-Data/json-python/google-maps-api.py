3257dd9cce4f90d74344b940a3e60ae1cee6e441
Rahul Rana <owlrana@Rahuls-MacBook-Pro.local>
2020-08-10 16:15:29 +0530

    updated with google api - no key

---
+++ rahulrana/Network-Data/json-python/google-maps-api.py
@@ -0,0 +1,34 @@
+# this program makes use of the google maps api and is consumer centric
+
+import urllib.request, urllib.parse, urllib.error
+import json
+
+servicourl = 'http://maps.googleapis.com/maps/api/geocode/json?' #gives us the json file for api use
+
+while True:
+    address = input('Enter Location: ')
+    if len(address) > 1: break
+
+    url = servicourl + urllib.parse.urlencode({'address': address }) #gives back and concatenates the servicourl with given address
+    # address spaces need to be turned to + and commans into % and other stuff that is done by this urrlib.parse.urlencode
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('=== Failure to Retreive ===')
+    print(data)
+    
+
+lat = js["results"][0]["geometry"]["location"]["lat"]
+lng = js["results"][0]["geometry"]["location"]["lng"]
+print('lat', lat, 'lng', lng)
+location = js['results'][0]['formatted_address']
+print(location)
--- rahulrana/Network-Data/json-python/json-python.py
+++ rahulrana/Network-Data/json-python/json-python.py
@@ -10,6 +10,9 @@ data = '''{
     }
 }'''
 
-info = json.loads(data) # loads the json format of data into python data structure for use (Data is a dictionary with name, phone, email out of which phone, email are dictionaries too)
+info = json.loads(data) 
+# loads the json format of data into python data structure for use 
+# (Data is a dictionary with name, phone, email out of which phone, email are dictionaries too)
+# into th eobject 'info'
 print('Name:', info["name"])
 print('Hide:', info["email"]["hide"])
