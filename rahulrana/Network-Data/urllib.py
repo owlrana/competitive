694fe469bc17ea36c530bdff64d728045efe0cf2
Rahul Rana <owlrana@Rahuls-MacBook-Pro.local>
2020-08-10 11:55:02 +0530

    Updating for mac os

--- rahulrana/Network-Data/urllib.py
+++ rahulrana/Network-Data/urllib.py
@@ -1,6 +1,7 @@
 # Using libraries that does all the socket work and makes web pages look more like a file
 # These require BeautifulSoup to be downloaded, which can be done from: https://pypi.python.org/pypi/beautifulsoup4
 # or can be locally installed through https://www.py4e.com/code3/bs4.zip and extracted into the local folder
+#updating existing lib
 
 import urllib.request, urllib.parse, urllib.error
 
