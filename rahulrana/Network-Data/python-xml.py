b63a838fdccaf7db73daf0a0adccd74f577fb5eb
Rahul Rana <owlrana@Rahuls-MacBook-Pro.local>
2020-08-10 13:09:16 +0530

    updated

--- rahulrana/Network-Data/python-xml.py
+++ rahulrana/Network-Data/python-xml.py
@@ -11,5 +11,5 @@ data = '''<person>
 </person>'''
 
 tree = ET.fromstring(data) #creates a tree and stores all the values in that tree so everything is organised
-print('Name:', tree.find('name').text)
-print('Attr:', tree.find('email').get('hide'))
+print('Name:', tree.find('name').text) #gives back the child, the text in it
+print('Attr:', tree.find('email').get('hide')) #gives back the attribute in it and NOT the text or other stuff
