6d4309845862b1ddc9b67b1959c671a5f078cda8
Rahul Rana <owlrana@Rahuls-MacBook-Pro.local>
2020-08-10 13:04:05 +0530

    reading data from xml file

---
+++ rahulrana/Network-Data/python-xml.py
@@ -0,0 +1,15 @@
+#creating xml data in python and then using a tree to access it 
+
+import xml.etree.ElementTree as ET
+
+data = '''<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +91 844 791 7137
+    </phone>
+    <email hide="nope"/>
+</person>'''
+
+tree = ET.fromstring(data) #creates a tree and stores all the values in that tree so everything is organised
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
