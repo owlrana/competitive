c6750ac7456f430d07b2b210f0da1f688863ec07
Rahul Rana <owlrana@Rahuls-MacBook-Pro.local>
2020-08-17 09:51:19 +0530

    updated

--- rahulrana/web-scraper.py
+++ rahulrana/web-scraper.py
@@ -1,3 +1,15 @@
 #building a web scraper (Make sure you do not scrape Amazon or Flipkart, will hut your connectivity and in some cases lose access to your machine) [Beautiful Soup]
 # www.crummy.com
 
+import urllib.request, urllib.error, urllib.parse
+
+from bs4 import BeautifulSoup #Need to have beautiful soup installed
+
+url = input('Enter - ')
+html = urllib.request.urlopen(ulr).read() #reads the whole page without a for loop or some other stuff
+soup = BeautifulSoup(html, 'html.parser') #turns html into a soup object named 'soup' with the format html.parser (recommended for HTMLs)
+
+#To retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
