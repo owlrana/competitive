480dcde15b613ac6536d8ac6853f0eb635dc4a5e
Rahul Rana <me.rahulrana@outlook.com>
2020-09-12 02:15:06 +0530

    Reverse Strings with & without Arrays

---
+++ rahulrana/MTICS-2/slicebackwards.py
@@ -0,0 +1,25 @@
+#Reverse a string without using arrays (using string slicing)
+
+##############################
+# You can either do this thing where we slice
+##############################
+txt = input('Input a string: ')
+newTxt = txt[::-1]
+print(newTxt)
+
+##############################
+# or do this approach which is kind of traditional
+##############################
+
+length = int(len(txt))
+print(length)
+revTxt = ""
+check = True
+
+while check == True:
+    revTxt = revTxt + txt[length - 1]
+    length = length - 1
+    if length == 0:
+        check = False
+
+print(revTxt)
