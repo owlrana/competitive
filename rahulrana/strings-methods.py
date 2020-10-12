4976e60b609da40214d4c17789c08f7cc0503706
Rahul Rana <me.rahulrana@outlook.com>
2020-10-12 21:50:50 +0530

    strings revise

--- rahulrana/strings-methods.py
+++ rahulrana/strings-methods.py
@@ -1,9 +1,18 @@
 # converting strings into upper lower, check etc
 
-test_string = "Rahul   Rana   is idiot"
+test_string = "Rahul Rana is idiot"
 
 print(test_string.capitalize())
 print(test_string.lower())
 print(test_string.isupper())
 print(len(test_string))
 print(test_string.join("1"))
+
+print(test_string[0])
+try:
+    print(test_string.index("idiot"))
+except:
+    print('')
+
+print(test_string.replace("Rahul Rana", "Vibhor Kumar"))
+
