b65c87ed7ec28ea962b031c5a63a455c2197ae3c
Rahul Rana <me.rahulrana@outlook.com>
2020-10-12 21:23:04 +0530

    restart review

---
+++ rahulrana/variables.py
@@ -0,0 +1,8 @@
+# To use variables in a story multiple times effectively as character names, age etc
+
+character_name = 'John'
+character_age = '35'
+print('There once was a man named ' + character_name +', ')
+print('he was 35 years old. ')
+print('He reallue liked the name' + character_name)
+print("but he didn't like being " + character_age)
