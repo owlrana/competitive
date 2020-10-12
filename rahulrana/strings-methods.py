3e2d1da514abc1089087737460de1d87257febf4
Rahul Rana <me.rahulrana@outlook.com>
2020-10-12 21:44:08 +0530

    string methods

---
+++ rahulrana/strings-methods.py
@@ -0,0 +1,9 @@
+# converting strings into upper lower, check etc
+
+test_string = "Rahul   Rana   is idiot"
+
+print(test_string.capitalize())
+print(test_string.lower())
+print(test_string.isupper())
+print(len(test_string))
+print(test_string.join("1"))
